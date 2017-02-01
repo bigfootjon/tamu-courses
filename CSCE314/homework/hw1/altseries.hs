9f6992f5799a84cb6fad790c8106e65023a6b15b
Jon Janzen <jjjonjanzen@gmail.com>
2017-02-01 09:24:05 -0600

    Update altseries.hs so that it is more terse

--- CSCE314/homework/hw1/altseries.hs
+++ CSCE314/homework/hw1/altseries.hs
@@ -1,4 +1,3 @@
 altSeries :: Num a => [a] -> a
-altSeries xs | mod (length xs) 2 == 0 && length xs > 0 = head xs - altSeries (tail xs)
-             | mod (length xs) 2 == 1 && length xs > 0 = head xs - altSeries (tail xs)
-             | otherwise = 0
+altSeries (x:xs) = x - altSeries xs
+altSeries [] = 0

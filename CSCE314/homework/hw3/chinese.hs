e8e553b32ca413f805810c4dfb400613f63a17e9
Jon Janzen <jjjonjanzen@gmail.com>
2017-02-04 13:34:41 -0600

    CSCE314 hw3 pr1

---
+++ CSCE314/homework/hw3/chinese.hs
@@ -0,0 +1,19 @@
+crt ls = (collapse (expand ls max) !! 0, max)
+       where max = maxval ls
+
+collapse ls | length ls >  2 = collapse ([x | x <- xs, y <- ys, x == y] : lss)
+            | length ls == 2 = [x | x <- xs, y <- ys, x == y]
+            | length ls <  2 = []
+            where xs  = head ls
+                  ys  = head $ tail ls
+                  lss = tail $ tail ls
+
+expand [] _ = []
+expand ls m = choices r d m : expand (tail ls) m
+            where r = fst (head ls)
+                  d = snd (head ls)
+
+choices r d m = [x | x <- [1..m], x `mod` d == r]
+
+maxval [] = 1
+maxval ((r, d):ls) = d * maxval ls

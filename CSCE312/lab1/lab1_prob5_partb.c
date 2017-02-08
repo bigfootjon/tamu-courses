b815cc24ea63e9532ebd8375beca0adb99b0b768
Jon Janzen <jjjonjanzen@gmail.com>
2017-02-07 19:34:48 -0600

    CSCE312 lab1 prob5 partb

--- CSCE312/lab1/lab1_prob5_partb.c
+++ CSCE312/lab1/lab1_prob5_partb.c
@@ -18,74 +18,48 @@
     #define CLOCKNAME CLOCK_PROCESS_CPUTIME_ID
 #endif
 
-int sensor_inputs;
+unsigned int DOS;
+unsigned int DSBF;
+unsigned int ER;
+unsigned int DC;
+unsigned int KIC;
+unsigned int DLC;
+unsigned int BP;
+unsigned int CM;
 
-enum input_offset {
-	DOS,DSBF,ER,DC,KIC,DLC,BP,CM
-};
+unsigned int BELL;
+unsigned int DLA;
+unsigned int BA;
 
-int actuator_outputs;
-
-enum output_offset {
-	BELL,DLA,BA
-};
-
-enum bitmask {
-	req123_filter =  14,
-	req123_check  =   4,
-	req4p1        =  33,
-	req4p2_filter =  49,
-	req4p2_check  =  32,
-	req5_filter   = 192,
-	req5_check    = 102,
-};
-
-void set_input_bit(enum input_offset shift) {
-	int temp;
-	scanf("%u", &temp);
-	sensor_inputs |= (temp & 1) << shift;
-}
-
-void set_output_bit(enum output_offset shift, unsigned int value) {
-	actuator_outputs |= (value & 1) << shift;
-}
-
-unsigned int get_output_bit(enum output_offset shift) {
-	return (actuator_outputs >> shift) & 1;
-}
 
 void read_inputs_from_ip_if(){
-	printf("-- INPUT --\n");
-	sensor_inputs = 0; // reset input;
-	printf("DOS  = "); set_input_bit(DOS);
-	printf("DSBF = "); set_input_bit(DSBF);
-	printf("ER   = "); set_input_bit(ER);
-	printf("DC   = "); set_input_bit(DC);
-	printf("KIC  = "); set_input_bit(KIC);
-	printf("DLC  = "); set_input_bit(DLC);
-	printf("BP   = "); set_input_bit(BP);
-	printf("CM   = "); set_input_bit(CM);
+        printf("-- INPUT --\n");
+	printf("DOS  = "); scanf("%u", &DOS);
+	printf("DSBF = "); scanf("%u", &DSBF);
+	printf("ER   = "); scanf("%u", &ER);
+	printf("DC   = "); scanf("%u", &DC);
+	printf("KIC  = "); scanf("%u", &KIC);
+	printf("DLC  = "); scanf("%u", &DLC);
+	printf("BP   = "); scanf("%u", &BP);
+	printf("CM   = "); scanf("%u", &CM);
 }
 
 void write_output_to_op_if() {
-	printf("-- OUTPUT --\n");
-	printf("BELL = %u\n", get_output_bit(BELL));
-	printf("DLA  = %u\n", get_output_bit(DLA));
-	printf("BA   = %u\n", get_output_bit(BA));
+        printf("-- OUTPUT --\n");
+	printf("BELL = %u\n", BELL);
+	printf("DLA  = %u\n", DLA);
+	printf("BA   = %u\n", BA);
 }
 
 
 //The code segment which implements the decision logic
-void control_action() {
-	actuator_outputs = 0;
-	// Else branches are implied because output is recet every cycle;
+void control_action(){
 	// combined requirements 1,2,3:
-	if ((sensor_inputs & req123_filter) == req123_check) set_output_bit(BELL, 1); // 14 means that bits representing 2,4,8 are set
+	if (ER && (!DSBF || !DC)) BELL = 1; else BELL = 0;
 	// requirement 4
-	if ((sensor_inputs & req4p1) == req4p1) set_output_bit(DLA, 1); // 33 means bits representing 1,32 are set
-	if ((sensor_inputs & req4p2_filter) == req4p2_check) set_output_bit(DLA, 1); // 49 means bits representing 1,16,32 are set
+	if ((DOS && DLC) || (!DOS && !KIC && DLC)) DLA = 1; else DLA = 0;
 	// requirement 5
-	if ((sensor_inputs & req5_filter) == req5_check) set_output_bit(BA, 1); // 192 means bits representing 64,128 are set
+	if (CM && BP) BA = 1; else BA = 0;
 }
 
 /* ---     You should not have to modify anything below this line ---------*/

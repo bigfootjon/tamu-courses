cae6d1e0442b2da878a5e25efa7fdb98423829e7
Jon Janzen <jonjanzen@tamu.edu>
2017-03-28 15:24:08 -0500

    CSCE312 Lab5 Pr4 added .s file

---
+++ CSCE312/lab5/lab5_prob4_main.s
@@ -0,0 +1,49 @@
+	.file	"lab5_prob4_main.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movq	%rsi, -16(%rbp)
+	movl	$0, %eax
+	call	print_hello
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.section	.rodata
+.LC0:
+	.string	"Hello, world"
+	.text
+	.globl	print_hello
+	.type	print_hello, @function
+print_hello:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$.LC0, %edi
+	call	puts
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	print_hello, .-print_hello
+	.ident	"GCC: (GNU) 6.3.0"
+	.section	.note.GNU-stack,"",@progbits

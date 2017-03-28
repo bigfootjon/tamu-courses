a69e0520780182ab5bc9c26af66b09606d155566
Jon Janzen <jjjonjanzen@gmail.com>
2017-03-28 15:21:59 -0500

    CSCE312 Lab5 Pr3 annotate ASM

--- CSCE312/lab5/lab5_prob3_2.s
+++ CSCE312/lab5/lab5_prob3_2.s
@@ -1,5 +1,7 @@
+; Metadata
 	.file	"lab5_prob3_2.c"
 	.section	.rodata
+; Static store of string
 .LC0:
 	.string	"The value of i is %d\n"
 	.text
@@ -7,6 +9,7 @@
 	.type	main, @function
 main:
 .LFB0:
+; begin setup of function
 	.cfi_startproc
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
@@ -16,18 +19,25 @@ main:
 	subq	$32, %rsp
 	movl	%edi, -20(%rbp)
 	movq	%rsi, -32(%rbp)
+; Create the variable i and assign 1 to it
 	movl	$1, -4(%rbp)
+; Increment i by 1
 	addl	$1, -4(%rbp)
+; Set up the call to printf
 	movl	-4(%rbp), %eax
 	movl	%eax, %esi
 	movl	$.LC0, %edi
 	movl	$0, %eax
+; Call printf
 	call	printf
+; Setup the return value
 	movl	$0, %eax
 	leave
 	.cfi_def_cfa 7, 8
+; Return from main function
 	ret
 	.cfi_endproc
+; Begin other metadata
 .LFE0:
 	.size	main, .-main
 	.ident	"GCC: (GNU) 6.3.0"

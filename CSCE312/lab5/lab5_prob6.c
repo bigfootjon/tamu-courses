5a5ce4c80111a607539b1cef9a7f00cbb9797de8
Jon Janzen <jjjonjanzen@gmail.com>
2017-03-28 15:54:20 -0500

    CSCE312 Lab5 Pr6 edit .c file

--- CSCE312/lab5/lab5_prob6.c
+++ CSCE312/lab5/lab5_prob6.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 
-int very_fast_function(int i) {
-	//asm("  ");
-	if ( (i*64 +1) > 1024 ) {
-		return i++;
-	} else {
-		return 0;
-	}
-}
+int very_fast_function(int);
+
+asm("\
+very_fast_function:\
+        pushq   %rbp;\
+        movq    %rsp, %rbp;\
+        movl    %edi, -4(%rbp);\
+        movl    -4(%rbp), %eax;\
+        sall    $6, %eax;\
+        addl    $1, %eax;\
+        cmpl    $1024, %eax;\
+        jle     .L2;\
+        movl    -4(%rbp), %eax;\
+        leal    1(%rax), %edx;\
+        movl    %edx, -4(%rbp);\
+        jmp     .L3;\
+.L2:\
+        movl    $0, %eax;\
+.L3:\
+        popq    %rbp;\
+        ret\
+");
 
 int main(int argc, char *argv[]) {
 	int i;

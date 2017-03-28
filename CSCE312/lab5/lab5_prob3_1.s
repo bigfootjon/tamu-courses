43356e78399b609c4d0ae0f57c29b263389c5217
Jon Janzen <jonjanzen@tamu.edu>
2017-03-28 15:14:03 -0500

    CSCE312 Lab5 problem3 added .s files

---
+++ CSCE312/lab5/lab5_prob3_1.s
@@ -0,0 +1,32 @@
+# Metadata:
+	.file	"lab5_prob3_1.c"
+	.section	.rodata
+# Static things
+.LC0:
+	.string	"Hello, world"
+	.text
+	.globl	main
+	.type	main, @function
+# The main function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movq	%rsi, -16(%rbp)
+	movl	$.LC0, %edi
+	call	puts 
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 6.3.0"
+	.section	.note.GNU-stack,"",@progbits

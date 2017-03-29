24304840b08d3590a38fa7ac54b7bd70acd62e1d
Jon Janzen <jjjonjanzen@gmail.com>
2017-03-29 09:03:39 -0500

    Updated prog.hs

--- CSCE314/homework/8/prog.hs
+++ CSCE314/homework/8/prog.hs
@@ -1,6 +1,6 @@
+module Main where
+
 import Prelude hiding (lookup)
-
-
 
 data WValue = VInt Int 
             | VBool Bool 
@@ -112,3 +112,24 @@ exec (Print exp         ) mem = putStr (show (eval exp mem)) >> return mem
 
 wprogram :: Parser WStmt
 
+
+main = do
+    args <- getArgs -- get the command line arguments
+
+    let (a:as) = args
+    let debug = a == "-d"
+    let fileName = if debug then head as else a
+
+    str <- readFile fileName
+
+    let prog = parse wprogram str 
+
+    let ast = case prog of 
+                [(p, [])] -> p
+                [(_, inp)] -> error ("Unused program text: " ++ take 256 inp) -- this helps in debugging
+                [] -> error "Syntax error"
+                _ -> error "Ambiguous parses"
+    result <- exec ast []
+
+    when debug $ print "AST:" >> print prog
+    when debug $ print "RESULT:" >> print result

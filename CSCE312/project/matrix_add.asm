55c247c3f31207610b01b172d3e962eb990a1067
Jon Janzen <jjjonjanzen@gmail.com>
2017-04-20 13:43:50 -0500

    Update a few things

--- CSCE312/project/matrix_add.asm
+++ CSCE312/project/matrix_add.asm
@@ -1,47 +1,47 @@
 # Setup matrix addresses
-regset V0, R0
-regset V4, R1
-regset V8, R4
+regset V0, R1
+regset V4, R2
+regset V8, R5
 
 # Copy first matrix into memory
-regset V0, R2
-memset R2, R0, V0
-regset V0, R2
-memset R2, R0, V1
-regset V0, R2
-memset R2, R0, V2
-regset V0, R2
-memset R2, R0, V3
+regset V0, R3
+memset R3, R1, V0
+regset V0, R3
+memset R3, R1, V1
+regset V0, R3
+memset R3, R1, V2
+regset V0, R3
+memset R3, R1, V3
 
 # Copy second matrix into memory
-regset V0, R2
-memset R2, R1, V0
-regset V1, R2
-memset R2, R1, V1
-regset V2, R2
-memset R2, R1, V2
-regset V3, R2
-memset R2, R1, V3
+regset V0, R3
+memset R3, R2, V0
+regset V1, R3
+memset R3, R2, V1
+regset V2, R3
+memset R3, R2, V2
+regset V3, R3
+memset R3, R2, V3
 
 # Add
-memld R2, R0, V0
 memld R3, R1, V0
-add R2, R3
-memset R3, R5, V0
+memld R4, R2, V0
+add R3, R4
+memset R4, R5, V0
 
-memld R2, R0, V1
 memld R3, R1, V1
-add R2, R3
-memset R3, R5, V1
+memld R4, R2, V1
+add R3, R4
+memset R4, R5, V1
 
-memld R2, R0, V2
 memld R3, R1, V2
-add R2, R3
-memset R3, R5, V2
+memld R4, R2, V2
+add R3, R4
+memset R4, R5, V2
 
-memld R2, R0, V3
 memld R3, R1, V3
-add R2, R3
-memset R3, R5, V3
+memld R4, R2, V3
+add R3, R4
+memset R4, R5, V3
 
 halt

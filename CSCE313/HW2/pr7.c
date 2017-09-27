96f5715e526f18127993e3786ff65e246a307699
Jonathan Janzen <jjjonjanzen@gmail.com>
2017-09-27 18:15:32 -0500

    Fixes to HW2

--- CSCE313/HW2/pr7.c
+++ CSCE313/HW2/pr7.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 
 int main(int argc, char** argv) {
 	if (argc < 2) return -1;
-	
+
 	char* str = argv[1];
-	int offset = strlen(str) - 1;
-	while (offset >= 0) {
-		if (!fork()) {
-			printf("%c", *(str+offset));
-			if (offset == 0) {
-				printf("\n");
-			}
-			return 0;
+	int offset = strlen(str);
+	while (offset-- && !fork()) {
+		fprintf(stderr, "%c", *(str+offset));
+		if (offset == 0) {
+			fprintf(stderr, "\n");
 		}
-		--offset;
 	}
+	wait(NULL);
 }
 

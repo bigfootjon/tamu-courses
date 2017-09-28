7c52f8600ed40ca425eff46707820365c0f10153
Jon Janzen <jjjonjanzen@gmail.com>
2017-09-28 14:09:32 -0500

    Finish homework2

--- CSCE313/HW2/string_invert.c
+++ CSCE313/HW2/string_invert.c
@@ -3,17 +3,23 @@
 #include <string.h>
 #include <sys/wait.h>
 
+void call(char* str, int offset) {
+	fprintf(stderr, "%c", *(str+offset));
+	if (offset == 0) {
+		fprintf(stderr, "\n");
+		return;
+	}
+	if (!fork()) {
+		call(str, offset-1);
+	}
+}
+
 int main(int argc, char** argv) {
 	if (argc < 2) return -1;
 
 	char* str = argv[1];
 	int offset = strlen(str);
-	while (offset-- && !fork()) {
-		fprintf(stderr, "%c", *(str+offset));
-		if (offset == 0) {
-			fprintf(stderr, "\n");
-		}
-	}
+	call(str, offset);
 	wait(NULL);
 }
 

46d76604dcda4b39506a5a5d924d107657e6cd5f
Jon Janzen <jjjonjanzen@gmail.com>
2017-09-28 16:23:03 -0500

    Start MS3 for MP1

--- CSCE313/MP1/my_allocator.c
+++ CSCE313/MP1/my_allocator.c
@@ -17,25 +17,51 @@
 #include "my_allocator.h"
 #include "free_list.h"
 
-char* base; // This is used to release the entire block of memory instead of wherever start is pointing to right now 
-FL_HEADER* start;
+typedef struct fl_tier {
+	FL_HEADER* fl;
+	unsigned int length;
+} FL_TIER;
+
+// This is used to release the entire block of memory instead of wherever start is pointing to right now
+char* base;
+int tiers = -1;
+FL_TIER* fl_array;
 
 unsigned int init_allocator(unsigned int _basic_block_size, unsigned int _length) {
-	if (start == 0) {
+	if (fl_array == 0) {
 		base = (char*)malloc(_length);
 		if (base == 0) {
 			return 0;
 		}
-		start = FL_init(base, _length);
+		
+		for (unsigned int i = _basic_block_size; i < _length; i *= 2) {
+			++tiers;
+		}
+
+		fl_array = (FL_TIER*)malloc(sizeof(FL_TIER) * tiers);
+		
+		unsigned int block_size = _basic_block_size;
+		for (unsigned int i = 0; i < tiers; ++i) {
+			block_size *= 2;
+			fl_array[i].fl = 0;
+			fl_array[i].length = block_size;
+		}
+		fl_array[tiers-1].fl = FL_init(base, _length);
 	}
 	return _length;
 }
 
 Addr my_malloc(size_t _length) {
-	if (start == 0) {
+	if (fl_array == 0) {
 		return 0;
 	}
-	FL_HEADER* current = start;
+	int tier = 0;
+	while (fl_array[tier].length < _length) ++tier;
+	if (tier > tiers) {
+		// Requesting more memory than we have, goodbye
+		return 0;
+	}
+	/*FL_HEADER* current = start;
 	while (current != 0) {
 		if (current->length >= _length) {
 			break;
@@ -52,9 +78,9 @@ Addr my_malloc(size_t _length) {
 		current->length = _length - sizeof(FL_HEADER);
 		start = FL_add(start, new_header);
 	}
-	return (Addr)(((char*)current) + sizeof(FL_HEADER));
+	return (Addr)(((char*)current) + sizeof(FL_HEADER));*/
 }
-
+/*
 int my_free(Addr _a) {
 	FL_HEADER* header = (FL_HEADER*)((char*)_a - sizeof(FL_HEADER));
 	header->next = 0;
@@ -62,7 +88,7 @@ int my_free(Addr _a) {
 	start = FL_add(start, header);
 	return 0;
 }
-
+*/
 int release_allocator() {
 	free(base);
 	return 0;
--- CSCE313/MP1/test.c
+++ CSCE313/MP1/test.c
@@ -5,15 +5,15 @@
 #include "free_list.h"
 
 int main(int arv, char** argc) {
-	printf("Initializing allocator with 100 bytes: %x\n", init_allocator(1, 100));
+	printf("Initializing allocator with 128 bytes (bbs=4): %x\n", init_allocator(4, 128));
 	printf("Size of FL_HEADER: %x\n", (unsigned int)sizeof(FL_HEADER));
 	void* pointer1 = my_malloc(1);
 	printf("Allocate memory(1): %x\n", (unsigned int)pointer1);
 	void* pointer2 = my_malloc(2);
 	printf("Allocate memory(2): %x\n", (unsigned int)pointer2);
 	printf("Allocate memory(L): %x\n", (unsigned int)my_malloc(100));
-	printf("Free     memory(2): %x\n", my_free(pointer2));
+	/*printf("Free     memory(2): %x\n", my_free(pointer2));
 	printf("Allocate memory(2): %x\n", (unsigned int)my_malloc(2));
-
+*/
 	release_allocator();
 }

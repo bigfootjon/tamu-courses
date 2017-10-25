35a6fdaf870e283a35b8534e57d9fc46cc504ee3
Jon Janzen <jonjanzen@tamu.edu>
2017-10-25 14:29:23 -0500

    Updated client code

--- CSCE313/MP3/client.C
+++ CSCE313/MP3/client.C
@@ -24,6 +24,7 @@
 #include <iostream>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 
 #include <errno.h>
 #include <unistd.h>
@@ -35,12 +36,21 @@
 using namespace std;
 
 const string QUIT_SIGIL = "MAGIC_QUIT_SIGIL_BETTATTI_IS_THE_BEST_PROF";
+
 Semaphore mutex_l(1);
+int worker_threads = 1;
+int request_threads = 0;
+int request_count = 100;
+BoundedBuffer* request_buffer;
+BoundedBuffer* response_buffer;
 
 int main(int argc, char * argv[]) {
-	int request_count = 100;
+	if (!fork()) {
+		system("./dataserver");
+		return 0;
+	}
+
 	int buffer_size = 2000;
-	int worker_threads = 1;
 	
 	char c;
 	while ((c = getopt (argc, argv, "n:b:w:")) != -1) {
@@ -65,36 +75,30 @@ int main(int argc, char * argv[]) {
 		}
 	}
 	
-	cout << "CLIENT STARTED:" << endl;
+	cout << "Main: CLIENT STARTED:" << endl;
 
-	cout << "Establishing control channel... " << flush;
+	cout << "Main: Establishing control channel... " << flush;
 	RequestChannel chan("control", RequestChannel::CLIENT_SIDE);
 	cout << "done." << endl;;
 
 	string hello_reply = chan.send_request("hello");
-	cout << "Reply to request 'hello' is '" << hello_reply << "'" << endl;
-	BoundedBuffer* request_buffer = new BoundedBuffer(buffer_size);
-	BoundedBuffer* response_buffer = new BoundedBuffer(buffer_size);
+	cout << "Main: (control) 'hello' -> '" << hello_reply << "'" << endl;
+	request_buffer = new BoundedBuffer(buffer_size);
+	response_buffer = new BoundedBuffer(buffer_size);
 	
 	vector<string> names = {"Joe Smith", "Jane Smith", "John Doe"};
 	
-	int request_threads = names.size();
-	int stat_threads = names.size();
+	request_threads = names.size();
 
 	vector<pthread_t> threads(names.size()*2 + worker_threads);
-	cout << "THREAD SIZE " << threads.size() << endl;
 	
 	for (int i = 0; i < names.size(); ++i) {
-		RequestDetails* req_attr = new RequestDetails(names[i], request_buffer, &request_threads, &request_count);
-		
 		pthread_t request_id = threads.at(i*2);
-		pthread_create(&request_id, NULL, request_thread, (void*)req_attr);
+		pthread_create(&request_id, NULL, request_thread, (void*)new string(names[i]));
 		threads[i*2] = request_id;
 		
-		StatsDetails* stat_attr = new StatsDetails(names[i], response_buffer);
-		
 		pthread_t stat_id = threads.at(i*2 + 1);
-		pthread_create(&stat_id, NULL, stat_thread, (void*)stat_attr);
+		pthread_create(&stat_id, NULL, stat_thread, (void*)new string(names[i]));
 		threads[i*2+1] = request_id;
 	}
 	
@@ -104,11 +108,8 @@ int main(int argc, char * argv[]) {
 		cout << "Main: (control) 'newthread' -> '" << chan_name << "'" << endl;
 		mutex_l.V();
 
-		RequestChannel* chan = new RequestChannel(chan_name, RequestChannel::CLIENT_SIDE);
-		WorkerDetails* attr = new WorkerDetails(request_buffer, response_buffer, chan, &worker_threads);
-		
 		pthread_t worker_id = threads.at(names.size()*2 + i);
-		pthread_create(&worker_id, NULL, worker_thread, (void*)attr);
+		pthread_create(&worker_id, NULL, worker_thread, (void*)new string(chan_name));
 		threads[names.size()*2 + i] = worker_id;
 	}
 
@@ -117,71 +118,70 @@ int main(int argc, char * argv[]) {
 	}
 	
 	string quit_reply = chan.send_request("quit");
-	cout << "Reply to request 'quit' is '" << quit_reply << "'" << endl;
+	cout << "Main: (control) 'quit' -> '" << quit_reply << "'" << endl;
+	
+	wait(NULL);
 }
 
 void * request_thread(void * _attr) {
-	RequestDetails *attr = ((RequestDetails*)_attr);
-	mutex_l.P();
-	cout << "Request: START " << attr->name << " " << *attr->request_count << endl;
-	mutex_l.V();
-	for (int i = 0; i < *attr->request_count; ++i) {
-		string request_string = "data " + attr->name;
+	string* name = (string*)_attr;
+	for (int i = 0; i < request_count; ++i) {
+		string request_string = "data " + *name;
 		mutex_l.P();
-		cout << "Request: (" << attr->name << ") " << request_string << endl;
+		cout << "Request: (" << *name << ") " << request_string << endl;
 		mutex_l.V();
-		attr->buffer->push(request_string);
+		request_buffer->push(request_string);
 	}
-	if (--(*attr->request_threads) == 0) {
-		attr->buffer->push(QUIT_SIGIL);
+	if (--request_threads == 0) {
+		request_buffer->push(QUIT_SIGIL);
 	}
-	delete attr;
+	delete name;
 	pthread_exit(0);
 	return 0;
 }
 
 void * worker_thread(void * _attr) {
-	WorkerDetails* attr = ((WorkerDetails*)_attr);
+	string* chan_name = (string*)_attr;
+	RequestChannel* chan = new RequestChannel(*chan_name, RequestChannel::CLIENT_SIDE);
 
 	for (;;) {
-		string request_string = attr->in_buffer->pop();
+		string request_string = request_buffer->pop();
 		if (request_string == QUIT_SIGIL) {
 			break;
 		}
-		string reply_string = attr->base_chan->send_request(request_string);
+		string reply_string = chan->send_request(request_string);
 		mutex_l.P();
-		cout << "Worker: '" << request_string << "' -> '" << reply_string << "'" << endl;
+		cout << "Worker:  (" << *chan_name << ") '" << request_string << "' -> '" << reply_string << "'" << endl;
 		mutex_l.V();
-		attr->out_buffer->push(reply_string);
+		response_buffer->push(reply_string);
 	}
-	attr->in_buffer->push(QUIT_SIGIL);
-	if (--(*attr->worker_threads) == 0) {
-		attr->out_buffer->push(QUIT_SIGIL);
+	request_buffer->push(QUIT_SIGIL);
+	if (--worker_threads == 0) {
+		response_buffer->push(QUIT_SIGIL);
 	}
-	string quit_response = attr->base_chan->send_request("quit");
+	string quit_response = chan->send_request("quit");
 	mutex_l.P();
-	cout << "Worker: 'quit' -> '" << quit_response << "'" << endl;
+	cout << "Worker:  (" << *chan_name << ") 'quit' -> '" << quit_response << "'" << endl;
 	mutex_l.V();
-	delete attr->base_chan;
-	delete attr;
+	delete chan_name;
 	pthread_exit(0);
 	return 0;
 }
 
 void * stat_thread(void * _attr) {
-	StatsDetails* attr = ((StatsDetails*)_attr);
+	string* name = (string*)_attr;
 
 	for (;;) {
-		string response_string = attr->buffer->pop();
+		string response_string = response_buffer->pop();
 		if (response_string == QUIT_SIGIL) {
 			break;
 		}
 		mutex_l.P();
-		//cout << "Stat: (" << attr->name << ") " << response_string << endl;
+		cout << "Stat:    (" << *name << ") " << response_string << endl;
 		mutex_l.V();
 	}
-	attr->buffer->push(QUIT_SIGIL);
-	delete attr;
+	response_buffer->push(QUIT_SIGIL);
+	delete name;
 	pthread_exit(0);
 	return 0;
 }

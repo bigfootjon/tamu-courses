ca563d1158c60f8b09cae1b208f38d5fed6a017b
Jon Janzen <jjjonjanzen@gmail.com>
2017-11-06 15:15:27 -0600

    Most of the work for HW4 pr4

---
+++ CSCE313/HW4/pr4.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void do_dir(char * dirname);
+void count_process();
+void list_process(char * dirname);
+
+int fd[2];
+
+int main(int argc, char ** argv) {
+	pipe(fd);
+	if (fork() == 0) {
+		count_process();
+		return 0;
+	} else {
+		if (argc != 2) {
+			printf("Please pass a directory as an agument\n");
+			return 1;
+		}
+
+		char *dirname = argv[1];
+		list_process(dirname);
+	}
+	return 0;
+}
+
+void do_dir(char *dirname) {
+	DIR* dir = opendir(dirname);
+	struct dirent * direntp;
+
+	while ((direntp = readdir(dir)) != NULL) {
+		char * fname = direntp->d_name;
+		if (strcmp(fname, ".") == 0 || strcmp(fname, "..") == 0) {
+			continue;
+		}
+		char path[1000];
+		snprintf(path, 1000, "%s/%s", dirname, fname);
+		printf("%s\n", path);
+		write(fd[1], path, strlen(path)+1);
+		DIR * subdir = opendir(path);
+		if (subdir != NULL) {
+			do_dir(path);
+		}
+	}
+	while (closedir(dir) == -1);
+}
+
+void list_process(char * dirname) {
+	close(fd[0]);
+
+	DIR * dirp;
+
+	dirp = opendir(dirname);
+	
+	if (dirp == NULL) { 
+		perror("Failed to open directory");
+		return;
+	}
+	while (closedir(dirp) == -1);
+	
+	do_dir(dirname);
+
+	close(fd[1]);
+	wait(NULL);
+}
+
+void count_process() {
+	close(fd[1]);
+	int count = 0;
+	while (1) {
+		char buffer[100];
+		int read_bytes = read(fd[0], buffer, sizeof(buffer));
+		if (read_bytes == 0) {
+			break;
+		}
+		++count;
+	}
+	printf("----- TOTAL ENTRIES: %d -----\n", count);
+}

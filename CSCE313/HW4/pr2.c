6b23c66caf57997227d6aa90bd364bba7c6f2c22
Jon Janzen <jjjonjanzen@gmail.com>
2017-11-06 13:49:19 -0600

    Finish HW4 Pr2

--- CSCE313/HW4/pr2.c
+++ CSCE313/HW4/pr2.c
@@ -1,25 +1,30 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/wait.h>
+
+#define READ 0
+#define WRITE 1
 
 int main() {
-	int wfd[2];
-	int rfd[2];
-	pipe(wfd);
-	pipe(rfd);
+	int request_fd[2];
+	int reply_fd[2];
+	pipe(request_fd);
+	pipe(reply_fd);
 	if (fork() == 0) { // Child
-		dup2(wfd[0], STDIN_FILENO);
-		dup2(rfd[1], STDOUT_FILENO);
+		close(reply_fd[READ]);
+		dup2(request_fd[READ], STDIN_FILENO);
+		dup2(reply_fd[WRITE], STDOUT_FILENO);
 		execv("./picodbd", NULL);
 	} else { // Parent
-		// Example requests:
+		close(request_fd[READ]);
+
+		// Example request:
 		char request[1];
 		request[0] = 'a';
-		while ((int)request[0] < 150) {
-			write(wfd[1], request, 1);
-			char reply[1];
-			int bytes_read = read(rfd[0], reply, 1);
-			printf("'%c' -> '%c'\n", request[0], reply[0]);
-			request[0] = reply[0];
-		}
+		write(request_fd[WRITE], request, 1);
+		char reply[1];
+		int bytes_read = read(reply_fd[READ], reply, 1);
+		
+		wait(NULL);
 	}
 }

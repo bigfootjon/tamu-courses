f101bf619dfc38549dd505eac53237f7e1bdb4e5
Jon Janzen <jonjanzen@tamu.edu>
2017-11-09 14:23:40 -0600

    Update batch_test.sh

--- CSCE313/MP4/batch_test.sh
+++ CSCE313/MP4/batch_test.sh
@@ -1,10 +1,10 @@
 for W in $(seq 1 20); do
 	for B in $(seq 1 20 3520); do
 		make fifo > /dev/null
-		start=$(date +%s)
+		srt=$(($(date +%s%N)/1000000))
 		./client -n 1000 -w $W -b $B > /dev/null
-		end=$(date +%s)
-		T=$((end-start))
+		end=$(($(date +%s%N)/1000000))
+		T=$((end-srt))
 		result="$W,$B,$T"
 
 		echo $result

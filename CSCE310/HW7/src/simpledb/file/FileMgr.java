b92e158f47bce3cebeacd3a71a4b41f714912ce5
Jon Janzen <jjjonjanzen@gmail.com>
2018-03-29 09:02:28 -0500

    Update FileMgr to use current working directory instead of home

--- CSCE310/HW7/src/simpledb/file/FileMgr.java
+++ CSCE310/HW7/src/simpledb/file/FileMgr.java
@@ -36,7 +36,7 @@ public class FileMgr {
     * @param dbname the name of the directory that holds the database
     */
    public FileMgr(String dbname) {
-      String homedir = System.getProperty("user.home");
+      String homedir = System.getProperty("user.dir");
       dbDirectory = new File(homedir, dbname);
       isNew = !dbDirectory.exists();
 

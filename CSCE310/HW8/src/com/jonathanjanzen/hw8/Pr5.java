0c5ca90c2e9a23f8fab3985358e6ca545ef4e10b
Jonathan Janzen <jjjonjanzen@gmail.com>
2018-04-15 10:57:57 -0500

    Add CSCE310 HW8 Pr5 class

---
+++ CSCE310/HW8/src/com/jonathanjanzen/hw8/Pr5.java
@@ -0,0 +1,10 @@
+package com.jonathanjanzen.hw8;
+
+public class Pr5 {
+    public static void main(String[] args) {
+        // QUERY: SELECT sname, dname, grade FROM STUDENT, DEPT, ENROLL, SECTION WHERE SId = StudentId AND SectId = SectionId and DId = MajorID;
+        // Slides from Query Processing, Use SimpleDb.{Plan, Scan}
+
+
+    }
+}

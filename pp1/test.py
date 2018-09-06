f954a370be7be6df7586c693eced8057a0adc555
Jon Janzen <jjjonjanzen@gmail.com>
2018-09-06 11:50:21 -0500

    Updated test.py to catch all test cases

--- pp1/test.py
+++ pp1/test.py
@@ -4,63 +4,88 @@ from __future__ import print_function
 
 import glob, os, sys, subprocess
 
-def get_test_names():
-    os.chdir("samples")
-    test_names = [file[:-5] for file in glob.glob("*.frag")]
-    os.chdir("..")
-    return test_names
+TEST_DIR = "samples"
 
+class TestCase:
+    @staticmethod
+    def get_all():
+        os.chdir(TEST_DIR)
+        tests = []
+        for ext in ['frag', 'decaf']:
+            tests.extend([TestCase(file) for file in glob.glob("*." + ext)])
+        os.chdir("..")
+        return tests
 
-def run_test(name):
-    did_pass = False
-    print("TEST: " + name)
-    with open('samples/' + name + '.frag', 'r') as compile_in:
-        p = subprocess.Popen(['./dcc'], stdin=compile_in, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    compiled = p.communicate()[0].decode("utf-8").replace("\r", "")
-    return_code = p.returncode
-    if return_code == 0:
-        print("  Compile: SUCCESS")
-    else:
-        print("  Compile: FAIL!")
-        if return_code == 255:
-            reason = "Compiler input error, if COMPARE passes below this is fine"
-        elif return_code == 11:
-            reason = "Segmentation fault! This is very bad"
+    @staticmethod
+    def get_by_name(name):
+        if os.path.isfile(os.path.join(TEST_DIR, name + '.frag')):
+            return TestCase(name + '.frag')
+        elif os.path.isfile(os.path.join(TEST_DIR, name + '.decaf')):
+            return TestCase(name + '.decaf')
         else:
-            reason = "Unknown"
-        print("    Return code: " + str(return_code) + " (" + reason + ")")
-    with open('samples/' + name + '.out', 'r') as given_out:
-        given = given_out.read().replace("\r", "")
-        if given == compiled:
-            print("  Compare: SUCCESS")
-            did_pass = True
-        else:
-            print("  Compare: FAIL!")
-            print("    Given output:")
-            print(given)
-            print("    Compiler output:")
-            print(compiled)
-    print()
-    return did_pass
+            raise Exception("No test case found in '" + TEST_DIR + "' with name '" + name + "' :(")
 
-def run_list(test_names):
+    def __init__(self, in_file):
+        self.name = in_file.split(".")[0]
+        self._in_file = in_file
+        self._out_file = self.name + ".out"
+
+    def run(self):
+        did_pass = False
+        print("TEST: " + self.name)
+        with open(os.path.join(TEST_DIR, self._in_file), 'r') as compile_in:
+            p = subprocess.Popen(['./dcc'], stdin=compile_in, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        compiled = p.communicate()[0].decode("utf-8").replace("\r", "")
+        return_code = p.returncode
+        if return_code == 0:
+            print("  Compile: SUCCESS")
+        else:
+            print("  Compile: FAIL!")
+            if return_code == 255:
+                reason = "Compiler input error, if COMPARE passes below this is fine"
+            elif return_code == 11:
+                reason = "Segmentation fault! This is very bad"
+            else:
+                reason = "Unknown"
+            print("    Return code: " + str(return_code) + " (" + reason + ")")
+        with open(os.path.join(TEST_DIR, self._out_file), 'r') as given_out:
+            given = given_out.read().replace("\r", "")
+            if given == compiled:
+                print("  Compare: SUCCESS")
+                did_pass = True
+            else:
+                print("  Compare: FAIL!")
+                print("    Given output:")
+                print(given)
+                print("    Compiler output:")
+                print(compiled)
+        print()
+        return did_pass
+
+
+
+def run_list(tests):
     passed = 0
     failed_names = []
-    for test_name in test_names:
-        if run_test(test_name):
+    for test in tests:
+        if test.run():
             passed += 1
         else:
-            failed_names.append(test_name)
+            failed_names.append(test.name)
     return passed, failed_names
 
 
 if __name__ == '__main__':
+    if not os.path.isfile('dcc'):
+        print("Please build the decaf compiler chain first with `make`")
+        exit(1)
+
     if len(sys.argv) > 1:
-        test_names = sys.argv[1:]
+        tests = [TestCase.get_by_name(name) for name in sys.argv[1:]]
     else:
-        test_names = get_test_names()
-    total = len(test_names)
-    passed, failed_names = run_list(test_names)
+        tests = TestCase.get_all()
+    total = len(tests)
+    passed, failed_names = run_list(tests)
 
     print("TOTAL: " + str(passed) + "/" + str(total) + " passed")
     if len(failed_names) > 0:

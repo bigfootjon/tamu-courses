a9cb4bd4f9c5ecca162826e37c620277af2d636b
Jon Janzen <jjjonjanzen@gmail.com>
2018-10-02 16:41:34 -0500

    Start work on pp4 (copied from pp3)

---
+++ pp4/ast_expr.cc
@@ -0,0 +1,124 @@
+/* File: ast_expr.cc
+ * -----------------
+ * Implementation of expression node classes.
+ */
+#include "ast_expr.h"
+#include "ast_type.h"
+#include "ast_decl.h"
+#include <string.h>
+#include "errors.h"
+
+
+IntConstant::IntConstant(yyltype loc, int val) : Expr(loc) {
+    value = val;
+}
+
+DoubleConstant::DoubleConstant(yyltype loc, double val) : Expr(loc) {
+    value = val;
+}
+
+BoolConstant::BoolConstant(yyltype loc, bool val) : Expr(loc) {
+    value = val;
+}
+
+StringConstant::StringConstant(yyltype loc, const char *val) : Expr(loc) {
+    Assert(val != NULL);
+    value = strdup(val);
+}
+
+Operator::Operator(yyltype loc, const char *tok) : Node(loc) {
+    Assert(tok != NULL);
+    strncpy(tokenString, tok, sizeof(tokenString));
+}
+CompoundExpr::CompoundExpr(Expr *l, Operator *o, Expr *r) 
+  : Expr(Join(l->GetLocation(), r->GetLocation())) {
+    Assert(l != NULL && o != NULL && r != NULL);
+    (op=o)->SetParent(this);
+    (left=l)->SetParent(this); 
+    (right=r)->SetParent(this);
+}
+
+CompoundExpr::CompoundExpr(Operator *o, Expr *r) 
+  : Expr(Join(o->GetLocation(), r->GetLocation())) {
+    Assert(o != NULL && r != NULL);
+    left = NULL; 
+    (op=o)->SetParent(this);
+    (right=r)->SetParent(this);
+}
+
+void CompoundExpr::CheckNode() {
+    if (left) left->Check();
+    op->Check();
+    right->Check();
+}
+
+void PostfixExpr::CheckNode() {
+    left->Check();
+    op->Check();
+}
+
+void This::CheckNode() {
+    // TODO This can only be within a class!
+}
+  
+ArrayAccess::ArrayAccess(yyltype loc, Expr *b, Expr *s) : LValue(loc) {
+    (base=b)->SetParent(this); 
+    (subscript=s)->SetParent(this);
+}
+
+void ArrayAccess::CheckNode() {
+    base->Check();
+    subscript->Check();
+}
+     
+FieldAccess::FieldAccess(Expr *b, Identifier *f) 
+  : LValue(b? Join(b->GetLocation(), f->GetLocation()) : *f->GetLocation()) {
+    Assert(f != NULL); // b can be be NULL (just means no explicit base)
+    base = b; 
+    if (base) base->SetParent(this); 
+    (field=f)->SetParent(this);
+}
+
+void FieldAccess::CheckNode() {
+    if (base) base->Check();
+    //if (LookupType(field->GetName()) == NULL) {
+    //    ReportError::IdentifierNotDeclared(field, reasonT::LookingForVariable);
+    //}
+}
+
+
+Call::Call(yyltype loc, Expr *b, Identifier *f, List<Expr*> *a) : Expr(loc)  {
+    Assert(f != NULL && a != NULL); // b can be be NULL (just means no explicit base)
+    base = b;
+    if (base) base->SetParent(this);
+    (field=f)->SetParent(this);
+    (actuals=a)->SetParentAll(this);
+}
+
+void Call::CheckNode() {
+    if (base) base->Check();
+    for (int i=0;i<actuals->NumElements(); ++i) {
+        actuals->Nth(i)->Check();
+    }
+}
+
+NewExpr::NewExpr(yyltype loc, NamedType *c) : Expr(loc) { 
+  Assert(c != NULL);
+  (cType=c)->SetParent(this);
+}
+
+void NewExpr::CheckNode() {
+    //cType->Check();
+}
+
+NewArrayExpr::NewArrayExpr(yyltype loc, Expr *sz, Type *et) : Expr(loc) {
+    Assert(sz != NULL && et != NULL);
+    (size=sz)->SetParent(this); 
+    (elemType=et)->SetParent(this);
+}
+
+void NewArrayExpr::CheckNode() {
+    size->Check();
+    elemType->Check();
+}
+

2cd67256c949f8e3d999f9e93f38c230b65b464b
Jonathan Janzen <jjjonjanzen@gmail.com>
2019-04-08 17:42:47 -0500

    Uncommit this later

--- CSCE451/A4/problem3/blue.c
+++ CSCE451/A4/problem3/blue.c
@@ -1,7 +1,25 @@
+#include <stdio.h>
 #include "include.h"
+
+typedef struct n {
+    n *left;
+    void *value;
+    n *right;
+} node_t;
+
+node_t graph;
 
 void blue() {
     blue_preflight();
-    wire_blue = false;
+    int result;
+    int v1;
+    int v2;
+    void *node_value;
+    node_t *cur = graph;
+    for (int i = 0; i <= 14; ++i) {
+
+    }
+    printf("\x1B[46m \x1B[0m\x1B[36m PROGRAMMING GATE ARRAY... ");
+    fflush(stdout);
     // TODO
 }
